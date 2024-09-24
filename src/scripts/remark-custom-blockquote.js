93d2af6ce2cefc72663911a1d6e449c2783a78e8
Imiloin <finvoler6@gmail.com>
2024-09-24 21:19:13 +0800

    fix: enable proper markdown rendering in non-alert blockquote

--- src/scripts/remark-custom-blockquote.js
+++ src/scripts/remark-custom-blockquote.js
@@ -28,22 +28,27 @@ export default function customBlockquote() {
                 return;
             }
 
-            let textContent = '';
-
-            // 遍历子节点，提取文本内容
-            visit(node, 'text', (child) => {
-                textContent += child.value;
-            });
-
-            node.type = 'html';
-            node.value = `
+            // 创建新的子节点，包含 SVG 图案和原始内容
+            const svgNode = {
+                type: 'html',
+                value: `
 <blockquote-no-alert>
 <svg fill="#5856D6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 14">
     <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
-</svg><br/>
-<p>${textContent}</p>
-</blockquote-no-alert>`;
-            node.children = [];
+</svg>
+                `
+            };
+
+            const closingTagNode = {
+                type: 'html',
+                value: `</blockquote-no-alert>`
+            };
+
+            // 将 SVG 节点插入到 blockquote 的第一个位置
+            node.children.unshift(svgNode);
+
+            // 将关闭标签节点插入到 blockquote 的最后一个位置
+            node.children.push(closingTagNode);
         });
     };
 }
